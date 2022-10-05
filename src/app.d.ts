e0eac5d16f4a4c72fa4839124fb0396fe0e37dc8
rootEnginear <rootEnginear@users.noreply.github.com>
2022-10-05 16:52:25 +0700

    feat: add process section

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,4 @@
+import type { ElementOrSelector, MotionKeyframesDefinition, AnimationListOptions } from 'motion';
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
@@ -6,4 +7,11 @@ declare namespace App {
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
+}
+
+declare global {
+	type TimelineSegment =
+		| [ElementOrSelector, MotionKeyframesDefinition, AnimationListOptions]
+		| [ElementOrSelector, MotionKeyframesDefinition];
+	type TimelineDefinition = TimelineSegment[];
 }
