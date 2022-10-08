f4ad08ec11cf98c14ca6ddebc5aaff6eafefd167
rootEnginear <rootEnginear@users.noreply.github.com>
2022-10-08 21:54:32 +0700

    feat: add first proto

--- src/components/sections/Investigate.svelte
+++ src/components/sections/Investigate.svelte
@@ -18,6 +18,108 @@
 	});
 </script>
 
-<section bind:this={el_section} id="investigate-section" class="h100 c wv-b1 wv-font-anuphan">
-	&lt;— Investigate Section (Soon) —&gt;
+<section bind:this={el_section} id="investigate-section" class="h100 c wv-font-anuphan">
+	<h2 class="wv-font-kondolar wv-h9 tc">
+		ร่างกฎหมายทั้งหมดที่ถูกเสนอในสมัยรัฐบาลประยุทธ์ จันทร์โอชา 2<br />
+		<span class="header-line2">
+			ทั้งหมด
+			<span class="wv-font-kondolar wv-h4 wv-font-black c-mint">300</span>
+			ฉบับ
+		</span>
+	</h2>
+
+	<div>
+		<div class="paper-container">
+			<div class="paper" />
+			<div class="paper" />
+			<div class="paper" />
+			<div class="line" />
+			<div class="paper" />
+			<div class="paper" />
+			<div class="paper" />
+		</div>
+		<div class="paper-container">
+			<div class="paper process" />
+			<div class="paper process" />
+			<div class="paper process" />
+			<div class="paper process" />
+			<div class="paper process" />
+			<div class="line" />
+			<div class="paper process" />
+			<div class="paper process" />
+			<div class="paper process" />
+			<div class="paper process" />
+			<div class="paper process" />
+		</div>
+		<div class="paper-container">
+			<div class="paper pass" />
+			<div class="paper pass" />
+			<div class="paper pass" />
+			<div class="paper pass" />
+			<div class="paper pass" />
+			<div class="paper pass" />
+			<div class="paper pass" />
+			<div class="paper pass" />
+		</div>
+	</div>
 </section>
+
+<style lang="scss">
+	.header-line2 {
+		display: inline-flex;
+		align-items: center;
+		gap: 1ch;
+	}
+
+	.paper-container {
+		display: flex;
+		margin-bottom: 4px;
+		padding-right: 10px;
+	}
+
+	.paper {
+		width: 20px;
+		height: 24px;
+
+		background: #000;
+		border: 1px #fff solid;
+
+		margin-right: -10px;
+
+		&.process {
+			background: linear-gradient(
+				-30deg,
+				#000,
+				#000 40.38%,
+				#fff 40.38%,
+				#fff 50%,
+				#000 50%,
+				#000 90.38%,
+				#fff 90.38%,
+				#fff 100%
+			);
+			background-size: 10.39px 6px;
+			background-repeat: repeat;
+		}
+
+		&.pass {
+			border: 1px #000 solid;
+			background: #fff;
+		}
+	}
+
+	.line {
+		position: relative;
+
+		&::after {
+			content: '';
+			position: absolute;
+			border-right: 3px #fff dotted;
+			z-index: 1;
+			display: block;
+			height: 100%;
+			transform: scale(1.5, 1.1) translateX(-25%);
+			left: 0;
+		}
+	}
+</style>
