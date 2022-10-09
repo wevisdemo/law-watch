fb2bf5d9ae03907aa8f319617fedda8764ac3d44
rootEnginear <suthep.chanchuphol@outlook.com>
2022-10-09 14:48:08 +0000

    Update gh-pages to output generated at 549b6fb

---
+++ _app/immutable/chunks/1-f18004c8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-f48d1247.js";export{t as component};
---
+++ _app/immutable/chunks/3-b77c6ad8.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-c8196999.js";import{default as t}from"../components/pages/about/_page.svelte-d39f302b.js";export{t as component,r as shared};
