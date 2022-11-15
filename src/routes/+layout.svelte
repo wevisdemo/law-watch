4f6e4f67639cf84d055a3816db31f2d274a9a25e
rootEnginear <rootEnginear@users.noreply.github.com>
2022-11-15 21:41:14 +0700

    fix: add cookie consent

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,9 +9,16 @@
 
 	import { weAreAlwaysHiring } from 'utils/we-are-hiring';
 
+	import WvCookieConsent from '@wevisdemo/ui/svelte/cookie-consent.svelte';
 	import WvFooter from '@wevisdemo/ui/svelte/footer.svelte';
 	import WvNavButton from '@wevisdemo/ui/svelte/nav-button.svelte';
 	import WvNavbar from 'components/wv/navbar.svelte';
+
+	const onCookieAccept = (option: Record<string, boolean>) => {
+		if ('Analytics' in option && option['Analytics']) {
+			// TODO: Add analytics
+		}
+	};
 
 	onMount(() => {
 		weAreAlwaysHiring();
@@ -19,6 +26,12 @@
 </script>
 
 <body class="wv-font-anuphan" class:bg-white={$page.route.id === '/about'}>
+	<WvCookieConsent
+		policyUrl="https://wevis.info/cookies_3/"
+		hasStrictlyNecessaryCookies
+		cookieOptions={['Analytics']}
+		onAccept={onCookieAccept}
+	/>
 	<WvNavbar>
 		<WvNavButton active={$page.route.id === '/'} dark onClick={() => goto(`${base}/`)}
 			>Home</WvNavButton
